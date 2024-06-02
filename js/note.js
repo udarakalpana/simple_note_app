bd69e48d3d6f682677ab913a62632050748e9f06
kalpana udara <kalpanaudara058@gmail.com>
2024-06-02 16:48:11 +0530

    Create simple note app using js localstorage

---
+++ js/note.js
@@ -0,0 +1,41 @@
+// Declare array for store notes
+const notes = JSON.parse(localStorage.getItem('notes')) || []
+
+// This part for accessing all the html elements into the js
+const noteForm = document.getElementById('note-form')
+const noteAddBtn = document.getElementById('note-add-btn')
+const noteInput = document.getElementById('note-input')
+const noteList = document.getElementById('note-list')
+
+
+// Event Listener for adding a note
+noteAddBtn.addEventListener('click', () => {
+    const note = noteInput.value
+    
+    if (note) {
+        notes.push(note)
+        localStorage.setItem('notes', JSON.stringify(notes))
+        renderNotes()
+        noteInput.value = ''
+    }
+})
+
+// This function for render in the UI locally store notes
+const renderNotes = () => {
+    noteList.innerHTML = ''
+    notes.map((note, index) => {
+        const listItem = document.createElement('li')
+        listItem.className = 'list-group-item d-flex mt-4'
+        listItem.innerHTML = `${note} <button type='button' class='btn btn-danger btn-sm' data-index='${index}'>Remove Note</button>`
+        noteList.appendChild(listItem)
+    })
+}
+
+// Event Listnere for remove single note
+noteList.addEventListener('click', (event) => {
+    const index = event.target.getAttribute('data-index')
+    notes.splice(index, 1)
+    renderNotes()
+})
+
+renderNotes()
